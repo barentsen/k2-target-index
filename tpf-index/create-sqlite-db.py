7dcd52af078dfd0b7b72aca806e7a4c1785f8afa
Geert Barentsen <geert@barentsen.be>
2016-02-06 18:04:42 -0800

    Adds a script to merge the campaigns into a single database

---
+++ tpf-index/create-sqlite-db.py
@@ -0,0 +1,34 @@
+"""Creates an SQLite database detailing all the K2 target pixel files.
+
+TODO
+----
+* Sort the final table by EPIC ID.
+* Add an index to the sqlite table?
+"""
+import glob
+import logging
+import sqlite3
+
+import pandas as pd
+
+
+log = logging.getLogger(__name__)
+log.setLevel("INFO")
+
+
+CSV_FILENAME = "k2-target-pixel-files.csv"
+SQLITE_FILENAME = "k2-target-pixel-files.db"
+
+
+if __name__ == "__main__":
+    log.info("Reading the data")
+    df = pd.concat([pd.read_csv(fn) for fn in glob.glob("k2*metadata.csv")])
+
+    # Write to the CSV file
+    log.info("Writing {}".format(CSV_FILENAME))
+    df.to_csv(CSV_FILENAME)
+
+    # Write the SQLite table
+    log.info("Writing {}".format(SQLITE_FILENAME))
+    con = sqlite3.connect(SQLITE_FILENAME)
+    df.to_sql(name='tpf', con=con, if_exists='replace')
