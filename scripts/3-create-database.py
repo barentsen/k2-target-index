758ac642a4c7109361930dadb280d080e6d93ab8
Geert Barentsen <geert@barentsen.be>
2016-02-21 19:47:16 -0800

    gzip the csv table; first WCS corners experiment

--- scripts/3-create-database.py
+++ scripts/3-create-database.py
@@ -6,11 +6,13 @@ import sqlite3
 
 import pandas as pd
 
+from astropy import wcs
+
 log = logging.getLogger(__name__)
 log.setLevel("INFO")
 
 # Output filenames
-CSV_FILENAME = "../k2-target-pixel-files.csv"
+CSV_FILENAME = "../k2-target-pixel-files.csv.gz"
 SQLITE_FILENAME = "../k2-target-pixel-files.db"
 
 
@@ -20,10 +22,29 @@ if __name__ == "__main__":
                     for fn
                     in glob.glob("intermediate-data/*metadata.csv")])
     df = df.sort_values("keplerid")
+    """
+    for idx, row in df.iterrows():
+        x, y = row['naxis1'], row['naxis2']
+        # Corners (ra, dec) of the stamp
+        metadata = {}
+        metadata['CTYPE1'] = 'RA---TAN'
+        metadata['CTYPE2'] = 'DEC--TAN'
+        for kw in ['naxis1', 'naxis2', 'crpix1', 'crpix2', 'crval1', 'crval2',
+                   'cdelt1', 'cdelt2', 'pc1_1', 'pc1_2', 'pc2_1', 'pc2_2',
+                   'crval1p', 'crval2p']:
+            metadata[kw] = row[kw]
+        mywcs = wcs.WCS(metadata)
+        corners = mywcs.all_pix2world([-0.5, -0.5, x-0.5, x-0.5],
+                                      [-0.5, y-0.5, y-0.5, -0.5],
+                                      0)
+        # Bounding box in equatorial coordinates
+        ra_min, ra_max = corners[0].min(), corners[0].max()
+        dec_min, dec_max = corners[1].min(), corners[1].max()
 
+    """
     # Write to the CSV file
     log.info("Writing {}".format(CSV_FILENAME))
-    df.to_csv(CSV_FILENAME, index=False)
+    df.to_csv(CSV_FILENAME, index=False, compression="gzip")
 
     # Write the SQLite table
     log.info("Writing {}".format(SQLITE_FILENAME))
